8758a6d0a614e802c144b2cb2537e062b80055f0
Darrell Mathews <smathewsbd@gmail.com>
2019-04-19 09:49:05 -0400

    initial commit

--- Grades/GradeStatistics.cs
+++ Grades/GradeStatistics.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 namespace Grades
 {
-    public class GradeStatistics
+public class GradeStatistics
     {
         public GradeStatistics()
         {
+            HighestGrade = 0;
+            LowestGrade = float.MaxValue;
         }
+
+        public float AverageGrade;
+        public float HighestGrade;
+        public float LowestGrade;
     }
 }
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -2,11 +2,21 @@
 
 namespace Grades
 {
-    class MainClass
+    class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Gradebook book = new Gradebook();
+            book.AddGrade(95);
+            book.AddGrade(89.7f);
+            book.AddGrade(75);
+
+            GradeStatistics stats = book.computeStatistics();
+            Console.WriteLine("GRADE REPORT");
+            Console.WriteLine("Your AVERAGE grade is " + stats.AverageGrade);
+            Console.WriteLine("Your HIGHEST grade is " + stats.HighestGrade);
+            Console.WriteLine("Your LOWEST grade is " + stats.LowestGrade);
+
         }
     }
 }
